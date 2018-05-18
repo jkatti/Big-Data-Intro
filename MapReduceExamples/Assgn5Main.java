96c31d62de1ceaae8d32076166d3689b4c342e41
jkatti <39218625+jkatti@users.noreply.github.com>
2018-05-19 00:00:41 +0530

    Add files via upload
    Submitting the code for Assignment 5

---
+++ MapReduceExamples/Assgn5Main.java
@@ -0,0 +1,42 @@
+package mapred.ex;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Assgn5Main {
+
+public static void main(String[] args) throws Exception {
+		
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "Assgn5Demo");
+		job.setJarByClass(Assgn5Main.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(Assgn5Mapper.class);
+		job.setReducerClass(Assgn5Reducer.class);
+		 
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0])); 
+		FileOutputFormat.setOutputPath(job,new Path(args[1]));
+		
+		
+		job.waitForCompletion(true);
+	
+
+	}
+	
+	
+}
