11decf08e3183a6b646ccfb9dcc2377a62234140
jkatti <39218625+jkatti@users.noreply.github.com>
2018-05-19 15:38:27 +0530

    Update Assgn6Main.java
    Added comments

--- MapReduceExamples/Assgn6Main.java
+++ MapReduceExamples/Assgn6Main.java
@@ -18,25 +18,29 @@ public class Assgn6Main {
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "Assgn6Demo");
 		job.setJarByClass(Assgn6Main.class);
-
+		
+		// Set the output format for the Mapper
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(Text.class);
-
+		
+		// Set the overall output classes to be used.
 		job.setOutputKeyClass(LongWritable.class);
 		job.setOutputValueClass(Text.class);
+		
+		// Set the Mapper and Reducer class files
 		job.setMapperClass(Assgn6Mapper.class);
-		//job.setReducerClass(Assgn6Reducer.class);
-		 
+		job.setReducerClass(Assgn6Reducer.class);
+		
+		// Set the overall output format
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
-
+		
+		// Set the input runtime arguments/parameters for the mapreduce program.
 		FileInputFormat.addInputPath(job, new Path(args[0])); 
 		FileOutputFormat.setOutputPath(job,new Path(args[1]));
 		
-		
 		job.waitForCompletion(true);
 	
-
 	}
 
 }
