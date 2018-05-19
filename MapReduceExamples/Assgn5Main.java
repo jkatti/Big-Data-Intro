4f0f7e6e70a80d85eafe48fd4dde62c91956ca5d
jkatti <39218625+jkatti@users.noreply.github.com>
2018-05-19 15:08:59 +0530

    Update Assgn5Main.java
    Added the comments indicating the functionality for the important lines.

--- MapReduceExamples/Assgn5Main.java
+++ MapReduceExamples/Assgn5Main.java
@@ -17,22 +17,27 @@ public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "Assgn5Demo");
 		job.setJarByClass(Assgn5Main.class);
-
+		
+		// Set the output format for the Mapper
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
-
+		
+		// Set the overall output classes to be used.
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+	
+		// Set the Mapper and Reducer class files
 		job.setMapperClass(Assgn5Mapper.class);
 		job.setReducerClass(Assgn5Reducer.class);
 		 
+		// Set the overall output format
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
+		// Set the input runtime arguments/parameters for the mapreduce program.
 		FileInputFormat.addInputPath(job, new Path(args[0])); 
 		FileOutputFormat.setOutputPath(job,new Path(args[1]));
-		
-		
+				
 		job.waitForCompletion(true);
 	
 
