054a0cf465b4cb623087455a855e466ef994039c
jkatti <39218625+jkatti@users.noreply.github.com>
2018-05-19 15:30:49 +0530

    Update Assgn5Mapper-2.java
    Added comments

--- MapReduceExamples/Assgn5Mapper-2.java
+++ MapReduceExamples/Assgn5Mapper-2.java
@@ -13,12 +13,20 @@ public class Assgn5Mapper extends Mapper<LongWritable, Text, Text, IntWritable> 
 	public void map(LongWritable key, Text value, Context context) 
 		throws IOException, InterruptedException {
 		
+		// Split the input record based on the delimeter "|". Because "|" is a special char, 
+		// We have to use it along with the 2 escape characters - "\\|"
 		String[] strArr = value.toString().split("\\|");
-		Text outputkey = new Text(strArr[3]);	
+		
+		// Make the state (index 3 in the input file) as the key so as to consolidate the number of units based on state.
+		Text outputkey = new Text(strArr[3]);
+		// For every record matching the key, assign the value of "1". 
+		//This will be used in the Reduce to sum to get to the total units for each state.
 		IntWritable outputvalue = new IntWritable(1);
 		
 		if (!(strArr[0].contains("NA"))) {
 			if (!(strArr[1].contains("NA"))) {
+		// Above 2 if statements ensure to ignore the records that have "NA" for company name and brand name.
+				//Filter out only records for brand "Onida" and write to context.
 				if (strArr[0].contains("Onida")) {
 					context.write(outputkey, outputvalue);
 				}				
